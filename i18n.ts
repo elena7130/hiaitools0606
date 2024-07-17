175a2a12c60466cf1a5383edfc0fc1e89894e000
elena7130 <qudongqin20@163.com>
2024-07-17 12:13:40 +0800

    修改国际化语言ja/zh-CH的表达

--- i18n.ts
+++ i18n.ts
@@ -9,7 +9,7 @@ export const languages = [
   },
   {
     code: 'ja-JP',
-    lang: 'jp',
+    lang: 'ja',
     label: '日本語',
   },
   {
@@ -39,12 +39,12 @@ export const languages = [
   },
   {
     code: 'zh-CN',
-    lang: 'cn',
+    lang: 'zh-CN',
     label: '简体中文',
   },
   {
     code: 'zh-TW',
-    lang: 'tw',
+    lang: 'zh-TW',
     label: '繁體中文',
   },
 ];
